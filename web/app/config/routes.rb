accd639e814fa9caebe5ff44e4753ff8ff04cc11
Vicky Gray <vickygray26@hotmail.com>
2016-03-29 00:40:53 +0100

    setup devise in back end

---
+++ web/app/config/routes.rb
@@ -0,0 +1,2 @@
+devise_for :users, :controllers => {sessions: 'user/sessions', registrations: 'user/registrations',
+  passwords: 'user/passwords'}
---
+++ web/app/controllers/user/registrations_controller.rb
@@ -0,0 +1,5 @@
+class User::RegistrationsController <
+  Devise::RegistrationsController
+    respond_to :json, :html
+  end
+  
